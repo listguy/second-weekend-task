86bf404671cefb724eb8e2e04fb8a09ad17abc9d
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-21 17:35:26 -0800

    declarat objects and first table css

---
+++ index.js
@@ -0,0 +1,95 @@
+const myObj = [
+    {
+        startedAt: new Date('2021-01-09:09:00'),
+        finishedAt: new Date('2021-01-09:14:00'),
+        tasksGiven: '6',
+        tasksFinished: '5',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Html'
+    },
+    {
+        startedAt: new Date('2021-01-09:09:00'),
+        finishedAt: new Date('2021-01-09:16:00'),
+        tasksGiven: '7',
+        tasksFinished: '4',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Javascript'
+    },
+    {
+        startedAt: new Date('2021-01-09:09:00'),
+        finishedAt: new Date('2021-01-09:18:00'),
+        tasksGiven: '9',
+        tasksFinished: '9',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Css'
+    },
+    {
+        startedAt: new Date('2021-01-09:12:00'),
+        finishedAt: new Date('2021-01-09:20:00'),
+        tasksGiven: '5',
+        tasksFinished: '2',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Array'
+    },
+    {
+        startedAt: new Date('2021-01-09:04:00'),
+        finishedAt: new Date('2021-01-09:12:00'),
+        tasksGiven: '6',
+        tasksFinished: '3',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Function'
+    },
+    {
+        startedAt: new Date('2021-01-09:10:00'),
+        finishedAt: new Date('2021-01-09:12:00'),
+        tasksGiven: '6',
+        tasksFinished: '1',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Object'
+    },
+    {
+        startedAt: new Date('2021-01-09:07:00'),
+        finishedAt: new Date('2021-01-09:12:00'),
+        tasksGiven: '4',
+        tasksFinished: '4',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'JSON'
+    },
+    {
+        startedAt: new Date('2021-01-09:13:00'),
+        finishedAt: new Date('2021-01-09:16:00'),
+        tasksGiven: '7',
+        tasksFinished: '5',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Methods'
+    },
+    {
+        startedAt: new Date('2021-01-09:09:00'),
+        finishedAt: new Date('2021-01-09:14:00'),
+        tasksGiven: '4',
+        tasksFinished: '3',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Loops'
+    },
+    {
+        startedAt: new Date('2021-01-09:04:00'),
+        finishedAt: new Date('2021-01-09:13:00'),
+        tasksGiven: '3',
+        tasksFinished: '3',
+        totalTime: '',
+        tasksFinishedPrecent: '',
+        topic: 'Github'
+    }
+];
+
+
+
